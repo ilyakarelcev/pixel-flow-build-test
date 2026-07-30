f944771459cb472a42d24440ae215a622fe8fda7
eccentricgames <53k@mail.ru>
2026-07-30 18:41:13 +0300

    Fix level editor Firebase exports

--- level-editor/firebase.js
+++ level-editor/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup as fbSignIn, signOut as fbSignOut } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
-import { getFirestore, collection as fbCol, doc as fbDoc, setDoc as fbSetDoc, getDocs as fbGetDocs, deleteDoc as fbDelDoc, query as fbQuery, orderBy as fbOrderBy } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+import { getFirestore, collection as fbCol, collectionGroup as fbColGroup, doc as fbDoc, setDoc as fbSetDoc, getDocs as fbGetDocs, deleteDoc as fbDelDoc, query as fbQuery, orderBy as fbOrderBy } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
 
 // TODO: Replace this with your actual Firebase config! 
 // Go to Firebase Console -> Project Settings -> General -> Web Apps
@@ -15,7 +15,7 @@ const firebaseConfig = {
 };
 
 let app, auth, provider, db;
-let signInWithPopup, signOut, collection, doc, setDoc, getDocs, deleteDoc, query, orderBy;
+let signInWithPopup, signOut, collection, collectionGroup, doc, setDoc, getDocs, deleteDoc, query, orderBy;
 
 try {
     if (firebaseConfig.apiKey !== "YOUR_API_KEY") {
@@ -27,6 +27,7 @@ try {
         signInWithPopup = fbSignIn;
         signOut = fbSignOut;
         collection = fbCol;
+        collectionGroup = fbColGroup;
         doc = fbDoc;
         setDoc = fbSetDoc;
         getDocs = fbGetDocs;
@@ -46,6 +47,7 @@ try {
     signInWithPopup = async () => alert("Configure Firebase parameters in firebase.js first!");
     signOut = async () => { };
     collection = () => { };
+    collectionGroup = () => { };
     doc = () => { };
     setDoc = async () => { };
     getDocs = async () => [];
@@ -54,4 +56,4 @@ try {
     orderBy = () => { };
 }
 
-export { auth, provider, db, signInWithPopup, signOut, collection, doc, setDoc, getDocs, deleteDoc, query, orderBy };
+export { auth, provider, db, signInWithPopup, signOut, collection, collectionGroup, doc, setDoc, getDocs, deleteDoc, query, orderBy };
